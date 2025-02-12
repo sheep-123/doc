bc1a972fd7fa60ebb62d667a56e96de1e06a3513
laim <laim@example>
2025-02-12 17:43:44 +0800

    del old version

--- front-end/src/App.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <nav>
-    <router-link to="/"></router-link>
-  </nav>
-<router-view>
-</router-view>
-</template>
-
--- front-end/src/views/chat.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <Header />
-  这是对话界面
-</template>
-
-<script setup>
-import Header from '@/components/header.vue';
-</script>
