5eeac4068f88dc4c1ad078188844eeaccc6ea1fb
qmj <qmjmmmm@163.com>
2025-02-13 16:50:40 +0800

    更新

--- frontend/src/views/file.vue
+++ frontend/src/views/file.vue
@@ -1,28 +1,11 @@
 <template>
-  <el-row :gutter="20" style="height: 40px; display: flex; align-items: center">
+  <el-row :gutter="20" style="height: 60px; display: flex; align-items: center">
     <!-- 自动补全搜索框 -->
-    <el-autocomplete
-      v-model="searchQuery"
-      :fetch-suggestions="querySearch"
-      clearable
-      class="inline-input w-50"
-      placeholder="请输入文档名字"
-      @select="handleSelect"
-      :trigger-on-focus="false"
-      style="width: 300px"
-    />
+    <el-autocomplete v-model="searchQuery" :fetch-suggestions="querySearch" clearable class="inline-input w-50"
+      placeholder="请输入文档名字" @select="handleSelect" :trigger-on-focus="false" style="width: 400px;margin-left: 20px;" />
 
-    <el-button type="primary" icon="Search" @click="searchDocuments"
-      >搜索</el-button
-    >
-    <el-button
-      type="info"
-      icon="Refresh"
-      @click="resetAll"
-      plain
-      style="color: black"
-      >重置</el-button
-    >
+    <el-button type="primary" icon="Search" @click="searchDocuments" style="margin-left: 20px;">搜索</el-button>
+    <el-button type="info" icon="Refresh" @click="resetAll" plain style="color: black">重置</el-button>
     <!-- <el-button type="success" @click="memorized" plain>已记忆</el-button> -->
     <!-- <el-button type="danger" @click="noMemory" plain>未记忆</el-button> -->
     <!-- 标签栏 -->
@@ -31,15 +14,9 @@
     <span style="margin-left: 10px">总文件数：{{ totalFiles }}</span>
     <span style="margin-left: 20px">总页数：{{ maxPage }}</span>
   </el-row>
-  <el-row :gutter="20" style="height: 40px">
-    <el-check-tag
-      v-for="tag in tags"
-      :key="tag"
-      :checked="selectedTagsMap[tag]"
-      class="multi-tag"
-      :type="selectedTagsMap[tag] ? 'success' : 'info'"
-      @change="handleTagChange(tag)"
-    >
+  <el-row :gutter="20" style="height: 40px;margin: 10px 10px;">
+    <el-check-tag v-for="tag in tags" :key="tag" :checked="selectedTagsMap[tag]" class="multi-tag"
+      :type="selectedTagsMap[tag] ? 'success' : 'info'" @change="handleTagChange(tag)">
       {{ tag }}
       <!-- <el-icon v-if="selectedTagsMap[tag]" class="selected-icon">
         <Check />
@@ -50,24 +27,11 @@
   <el-row :gutter="20">
     <!-- Add New 按钮 -->
     <el-col :span="100">
-      <el-upload
-        action="/file/upload"
-        :before-upload="beforeUpload"
-        :on-success="handleSuccess"
-        :on-progress="handleProgress"
-        :on-error="handleError"
-        accept=".pdf"
-        :show-file-list="false"
-        :multiple="true"
-        :disabled="isMemoryProcessing"
-        drag
-        class="upload-card"
-      >
-        <el-card
-          class="add-el-card"
-          :style="{ width: '280px', height: '256px' }"
-          :class="{ uploading: loading, disabled: isMemoryProcessing }"
-        >
+      <el-upload action="/file/upload" :before-upload="beforeUpload" :on-success="handleSuccess"
+        :on-progress="handleProgress" :on-error="handleError" accept=".pdf" :show-file-list="false" :multiple="true"
+        :disabled="isMemoryProcessing" drag class="upload-card">
+        <el-card class="add-el-card" :style="{ width: '280px', height: '256px' }"
+          :class="{ uploading: loading, disabled: isMemoryProcessing }">
           <el-icon>
             <Plus />
           </el-icon>
@@ -93,49 +57,18 @@
         <!-- 添加编辑图标和输入框 -->
         <div class="tab-edit">
           <template v-if="!doc.tags && !doc.isEditing">
-            <el-button
-              type="info"
-              plain
-              icon="Edit"
-              @click="editTag(doc)"
-              style="color: black"
-              >标签</el-button
-            >
+            <el-button type="info" plain icon="Edit" @click="editTag(doc)" style="color: black">标签</el-button>
           </template>
           <template v-else-if="!doc.isEditing">
-            <el-button
-              type="info"
-              plain
-              icon="Edit"
-              @click="editTag(doc)"
-              style="color: black"
-              >{{ doc.tags }}</el-button
-            >
+            <el-button type="info" plain icon="Edit" @click="editTag(doc)" style="color: black">{{ doc.tags
+              }}</el-button>
           </template>
           <template v-if="doc.isEditing">
-            <el-input
-              v-model="doc.tags"
-              @blur="saveTag(doc)"
-              @keyup.enter="saveTag(doc)"
-              ref="tagText"
-              style="width: 100px"
-              :autofocus="true"
-            />
+            <el-input v-model="doc.tags" @blur="saveTag(doc)" @keyup.enter="saveTag(doc)" ref="tagText"
+              style="width: 100px" :autofocus="true" />
             <div class="button">
-              <el-button
-                type="info"
-                @click="saveTag(doc)"
-                plain
-                style="color: black"
-                >√</el-button
-              >
-              <el-button
-                type="info"
-                @click="cancelEdit(doc)"
-                plain
-                style="color: black"
-                >x</el-button
-              >
+              <el-button type="info" @click="saveTag(doc)" plain style="color: black">√</el-button>
+              <el-button type="info" @click="cancelEdit(doc)" plain style="color: black">x</el-button>
             </div>
           </template>
         </div>
@@ -146,17 +79,8 @@
           </el-tooltip>
         </div>
         <div class="content">
-          <img
-            v-if="doc.type === 'image'"
-            :src="doc.docURL"
-            alt="Document Preview"
-            class="doc-preview"
-          />
-          <embed
-            v-else-if="doc.type === 'pdf'"
-            :src="doc.docURL"
-            class="doc-preview"
-          />
+          <img v-if="doc.type === 'image'" :src="doc.docURL" alt="Document Preview" class="doc-preview" />
+          <embed v-else-if="doc.type === 'pdf'" :src="doc.docURL" class="doc-preview" />
           <div class="ms" v-if="doc.parse">
             解析进度：{{ doc.parseProgress }}%
           </div>
@@ -186,17 +110,10 @@
         <!-- 毛玻璃上传中-->
         <div v-if="doc.isUploaded" class="glass-overlay">
           <div class="progress-info">
-            <el-progress
-              v-if="doc.isUploaded"
-              :percentage="doc.uploadProgress"
-              :status="doc.uploadStatus"
-              class="upload-progress"
-              stroke-linecap="square"
-              type="dashboard"
-            >
+            <el-progress v-if="doc.isUploaded" :percentage="doc.uploadProgress" :status="doc.uploadStatus"
+              class="upload-progress" stroke-linecap="square" type="dashboard">
               <template #default="{ percentage }">
-                <span class="percentage-value">{{ percentage }}%</span>
-                <span class="percentage-label">Progressing</span>
+                <span class="percentage-value">上传进度:{{ doc.uploadProgress }}%</span>
               </template>
             </el-progress>
           </div>
@@ -209,10 +126,7 @@
         </div>
 
         <!-- 添加全局蒙层 -->
-        <div
-          v-if="isMemoryProcessing && !doc.isProcessing"
-          class="glass-overlay memory-processing"
-        >
+        <div v-if="isMemoryProcessing && !doc.isProcessing" class="glass-overlay memory-processing">
           <div class="progress-info">
             <div class="progress-text">
               <el-icon>
@@ -233,18 +147,13 @@
   </el-dialog>
   <!-- 分页 -->
   <div class="page">
-    <el-pagination
-      layout="prev, pager, next"
-      :total="1000"
-      :page-size="num"
-      @current-change="pageChange"
-      :page-count="maxPage"
-    />
+    <el-pagination layout="prev, pager, next" :total="1000" :page-size="num" @current-change="pageChange"
+      :page-count="maxPage" />
   </div>
 </template>
 
 <script setup>
-import { ref, onMounted, getCurrentInstance, nextTick, reactive } from 'vue';
+import { ref, onMounted, getCurrentInstance, nextTick, reactive, onUnmounted } from 'vue';
 import { ElMessage } from 'element-plus';
 // import Header from '@/components/header.vue';
 
@@ -268,34 +177,34 @@ const page = ref(1);
 const num = ref(24);
 const maxPage = ref(0);
 const totalFiles = ref(0);
+
+// 在 setup 顶部添加公共方法
+const mapDocumentData = (item) => ({
+  title: item.file_name,
+  type: 'pdf',
+  docURL: item.file_path,
+  isProcessing: false,
+  isUploaded: false,
+  uploadProgress: 0,
+  status: [1, 2].includes(item.status) ? '未记忆' : '已记忆',
+  tags: item.tag,
+  parse: false,
+  parseProgress: 0,
+  finish: true,
+  id: item.id,
+  isMemory: item.status === 3,
+  isEditing: false,
+  progress: item.status === 3 ? 100 : 0
+});
+
 // 获取文件列表
 const getFileList = async () => {
-  var result = await proxy.$GET({
+  const result = await proxy.$GET({
     url: 'file/getfilelist',
     params: { page: page.value, num: num.value }
   });
 
-  result.data.data.map((item) => {
-    documents.value.push({
-      title: item.file_name,
-      type: 'pdf',
-      docURL: item.file_path,
-      isProcessing: false,
-      isUploaded: false,
-      uploadProgress: 0,
-      uploadStatus: 'success',
-      status: item.status == 2 || item.status == 1 ? '未记忆' : '已记忆',
-      tags: item.tag,
-      parse: false,
-      parseProgress: 0,
-      finish: true,
-      id: item.id,
-      isMemory: item.status == 3 ? true : false,
-      isEditing: false, // 新增状态控制编辑模式
-      progress: item.status == 3 ? 100 : 0
-    });
-  });
-
+  documents.value = result.data.data.map(mapDocumentData);
   maxPage.value = Math.ceil(result.data.count / num.value);
   totalFiles.value = result.data.count;
 };
@@ -325,27 +234,40 @@ const getQuerySearch = async () => {
   }
 };
 
-// websocket
+const MAX_RETRIES = 3;
+let retryCount = 0;
+
 const connectWebsocket = async () => {
-  if (ws.value) {
-    ws.value.close();
+  try {
+    if (ws.value) {
+      ws.value.close();
+    }
+
+    ws.value = new WebSocket('ws://192.168.6.137:8000/ws');
+    setupWebSocketHandler();
+
+    // 添加重连监听
+    ws.value.onclose = () => {
+      if (retryCount < MAX_RETRIES) {
+        retryCount++;
+        setTimeout(connectWebsocket, 2000 * retryCount);
+      }
+    };
+
+    ws.value.onopen = () => {
+      // console.log('WebSocket连接已建立');
+    };
+
+    ws.value.onerror = (error) => {
+      console.error('WebSocket错误:', error);
+    };
+  } catch (error) {
+    console.error('WebSocket连接失败:', error);
   }
-
-  ws.value = new WebSocket('ws://192.168.6.137:8000/ws');
-  setupWebSocketHandler(); // 初始化消息处理器
-
-  ws.value.onopen = () => {
-    console.log('WebSocket连接已建立');
-  };
-
-  ws.value.onerror = (error) => {
-    console.error('WebSocket错误:', error);
-  };
 };
 
 const searchQuery = ref('');
 const tags = ref([]);
-const activeTag = ref('');
 
 const selectedTagsMap = reactive({});
 const selectedTags = ref([]);
@@ -353,7 +275,6 @@ const selectedTags = ref([]);
 // 标签状态变化处理
 const handleTagChange = (tag) => {
   selectedTagsMap[tag] = !selectedTagsMap[tag];
-  console.log(selectedTagsMap);
   updateSelectedTags();
   filterDocuments();
 };
@@ -362,7 +283,6 @@ const handleTagChange = (tag) => {
 const updateSelectedTags = () => {
   // object.keys()获取对象所有的key
   // filter()过滤值为true的key
-  console.log(Object.keys(selectedTagsMap));
   selectedTags.value = Object.keys(selectedTagsMap).filter(
     (tag) => selectedTagsMap[tag]
   );
@@ -375,29 +295,12 @@ const filterDocuments = async () => {
     params: {
       tag: selectedTags.value.join(','),
       page: page.value,
-      num: num.value
+      num: num.value,
+      keywords: searchQuery.value ?? ''
     }
   });
   documents.value = [];
-  result.data.data.map((item) => {
-    documents.value.push({
-      title: item.file_name,
-      type: 'pdf',
-      docURL: item.file_path,
-      isProcessing: false,
-      isUploaded: false,
-      uploadProgress: 0,
-      status: item.status == 2 || item.status == 1 ? '未记忆' : '已记忆',
-      tags: item.tag,
-      parse: false,
-      parseProgress: 0,
-      finish: true,
-      id: item.id,
-      isMemory: item.status == 3 ? true : false,
-      isEditing: false, // 新增状态控制编辑模式
-      progress: item.status == 3 ? 100 : 0
-    });
-  });
+  documents.value = result.data.data.map(mapDocumentData);
 
   maxPage.value = Math.ceil(result.data.count / num.value);
   totalFiles.value = result.data.count;
@@ -464,13 +367,14 @@ const setupWebSocketHandler = () => {
   ws.value.onmessage = (event) => {
     const data = JSON.parse(event.data);
     const targetDoc = documents.value.find(d => d.id === data.id);
-    
+
     if (targetDoc) {
       targetDoc.parseProgress = Math.floor(data.progress);
       if (data.progress === 100) {
         targetDoc.parse = false;
         targetDoc.finish = true;
         parseQueue.value.shift();
+        isParsing.value = false;
         startNextParse();
       }
     }
@@ -483,7 +387,7 @@ const startNextParse = () => {
 
   isParsing.value = true;
   const currentDoc = parseQueue.value[0];
-  
+
   const targetDoc = documents.value.find(d => d.id === currentDoc.id);
   if (targetDoc && !targetDoc.finish) {
     targetDoc.parse = true;
@@ -493,6 +397,9 @@ const startNextParse = () => {
       id: currentDoc.id,
       data: currentDoc.base64Data
     });
+  } else {
+    isParsing.value = false;
+    startNextParse();
   }
 };
 
@@ -578,23 +485,13 @@ const deepMemory = async (doc) => {
 const simulateProgress = async (doc) => {
   if (!doc.isProcessing) {
     doc.isProcessing = true;
-    const timer = setInterval(async () => {
-      doc.progress += 10; // 每秒增加10%
-      if (doc.progress >= 100) {
-        clearInterval(timer);
-        doc.isProcessing = false;
-        var result = await proxy.$GET({
-          url: 'file/memory',
-          params: { id: doc.id }
-        });
+    sendViaWebSocket({
+      type: 'deepMemory',
+      filename: doc.title,
+      id: doc.id,
+    });
 
-        if (result.code === 1) {
-          doc.status = '已记忆'; // 更新状态文本
-          doc.isMemory = true; // 添加一个标记表示已记忆
-        }
-        isMemoryProcessing.value = false; // 关闭蒙层
-      }
-    }, 1000);
+
   }
 };
 
@@ -655,24 +552,7 @@ const pageChange = async (num) => {
   });
 
   documents.value = [];
-  result.data.data.map((item) => {
-    documents.value.push({
-      title: item.file_name,
-      type: 'pdf',
-      docURL: item.file_path,
-      isProcessing: false,
-      isUploaded: false,
-      progress: item.status == 2 || item.status == 1 ? 0 : 100,
-      status: item.status == 2 || item.status == 1 ? '未记忆' : '已记忆',
-      tags: item.tag,
-      parse: false,
-      parseProgress: 0,
-      finish: true,
-      id: item.id,
-      isMemory: item.status == 3 ? true : false,
-      isEditing: false // 新增状态控制编辑模式
-    });
-  });
+  documents.value = result.data.data.map(mapDocumentData);
 };
 
 // 搜索
@@ -680,31 +560,14 @@ const searchDocuments = async () => {
   var result = await proxy.$GET({
     url: 'file/getfilelist',
     params: {
-      keywords: searchQuery.value,
-      tag: activeTag.value,
+      keywords: searchQuery.value ?? "",
+      tag: selectedTags.value.join(','),
       page: 1,
       num: num.value
     }
   });
   documents.value = [];
-  result.data.data.map((item) => {
-    documents.value.push({
-      title: item.file_name,
-      type: 'pdf',
-      docURL: item.file_path,
-      isProcessing: false,
-      isUploaded: false,
-      progress: item.status == 2 || item.status == 1 ? 0 : 100,
-      status: item.status == 2 || item.status == 1 ? '未记忆' : '已记忆',
-      tags: item.tag,
-      parse: false,
-      parseProgress: 0,
-      finish: true,
-      id: item.id,
-      isMemory: item.status == 3 ? true : false,
-      isEditing: false // 新增状态控制编辑模式
-    });
-  });
+  documents.value = result.data.data.map(mapDocumentData);
 
   maxPage.value = Math.ceil(result.data.count / num.value);
   totalFiles.value = result.data.count;
@@ -729,7 +592,7 @@ const createFilter = (queryString) => {
 // 重置
 const resetAll = async () => {
   searchQuery.value = '';
-  activeTag.value = '';
+  selectedTags.value = []
   page.value = 1;
   var result = await proxy.$GET({
     url: 'file/getfilelist',
@@ -738,24 +601,7 @@ const resetAll = async () => {
 
   documents.value = [];
 
-  result.data.data.map((item) => {
-    documents.value.push({
-      title: item.file_name,
-      type: 'pdf',
-      docURL: item.file_path,
-      isProcessing: false,
-      isUploaded: false,
-      progress: item.status == 1 || item.status == 2 ? 0 : 100,
-      status: item.status == 2 || item.status == 1 ? '未记忆' : '已记忆',
-      tags: item.tag,
-      parse: false,
-      parseProgress: 0,
-      finish: true,
-      id: item.id,
-      isMemory: item.status == 3 ? true : false,
-      isEditing: false // 新增状态控制编辑模式
-    });
-  });
+  documents.value = result.data.data.map(mapDocumentData);
 
   maxPage.value = Math.ceil(result.data.count / num.value);
 };
@@ -772,7 +618,7 @@ const memorized = async () => {
       page: 1,
       num: num.value,
       status: 3,
-      tag: activeTag.value,
+      tag: selectedTags.value.join(','),
       keywords: searchQuery.value
     }
   });
@@ -810,31 +656,13 @@ const noMemory = async () => {
       page: 1,
       num: num.value,
       status: 1 || 2,
-      tag: activeTag.value,
+      tag: selectedTags.value.join(','),
       keywords: searchQuery.value
     }
   });
   if (result.code == 1) {
     documents.value = [];
-    result.data.data.map((item) => {
-      documents.value.push({
-        title: item.file_name,
-        type: 'pdf',
-        docURL: item.file_path,
-        isProcessing: false,
-        isUploaded: false,
-        uploadProgress: 0,
-        uploadStatus: 'success',
-        status: item.status == 2 || item.status == 1 ? '未记忆' : '已记忆',
-        tags: item.tag,
-        parse: false,
-        finish: true,
-        id: item.id,
-        isMemory: item.status == 3 ? true : false,
-        isEditing: false, // 新增状态控制编辑模式
-        progress: 0
-      });
-    });
+    documents.value = result.data.data.map(mapDocumentData);
   } else {
     ElMessage.error('暂无未记忆的文档');
   }
@@ -856,46 +684,17 @@ const truncateFileName = (name, maxLength) => {
     baseName.length - keepLength
   )}${extension}`;
 };
+
+// 添加清理逻辑
+onUnmounted(() => {
+  if (ws.value) {
+    ws.value.close();
+    ws.value = null;
+  }
+  parseQueue.value = [];
+});
 </script>
 
 <style scoped>
 @import url('/assets/css/file.css');
-
-.edit-icon {
-  margin-left: 10px;
-  cursor: pointer;
-}
-
-.tag-container {
-  margin: 12px 0;
-  flex-wrap: wrap;
-  row-gap: 8px;
-}
-
-.multi-tag {
-  margin-right: 8px;
-  margin-bottom: 8px;
-  padding: 8px 15px;
-  border-radius: 15px;
-  transition: all 0.2s ease;
-  position: relative;
-  cursor: pointer;
-
-  &:hover {
-    transform: translateY(-2px);
-    box-shadow: 0 2px 6px rgba(0, 0, 0, 0.1);
-  }
-}
-
-.selected-icon {
-  margin-left: 5px;
-  font-size: 12px;
-  vertical-align: middle;
-}
-
-/* 选中状态样式 */
-.el-check-tag.is-checked {
-  background-color: var(--el-color-success-light-9);
-  border-color: var(--el-color-success);
-}
 </style>
